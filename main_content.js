09dc82a176de9169e35283d5c016f90f3d992357
zawa159 <shinozawakouta159@gmail.com>
2023-07-17 22:32:10 +0900

    初回接続時に動作しない問題を修正
    youtubeでは通常のページ遷移が行われないため、
    コンテンツスクリプトが実行されない(ページロード時のみ実行される)
    そのため、bodyタグを監視して追加・削除があり次第実施

--- main_content.js
+++ main_content.js
@@ -1,23 +1,33 @@
-//グッドボタン等のメニューを２列へ変更
+//グッドボタン等のメニューを1段下へ変更
 const menu_newline = () => {
   //menuを２段に変更
-  const metadata = document.getElementById('top-row')
-  if (metadata.style.display != 'inline') {
-    metadata.style.display = 'inline';
-    console.log(metadata.style.display); 
-  }
+  const metadata = document.getElementById('top-row');
+  metadata.style.display = 'inline';
+  console.log('menu_newline  Loading complete');
 }
 
-/*
-const hide_share_button = () => {
-  //共有ボタンを取得
-  let shareButton = document.querySelectorAll('ytd-button-renderer')
- 
-  //共有を非表示へ
-  shareButton.style.display = "none";
+//コールバック関数
+const callback = (mutations) => {
+  //第一引数 mutations は変化の内容を表す MutationRecord オブジェクトの配列
+  mutations.forEach((mutation) => {
+    menu_newline();
+  })
 }
-*/
-// ページ読み込み後に実行するよう設定
-//window.addEventListener('load', () => menu_newline(), false);
-//window.onload = menu_newline;
-window.ontransitionend  = menu_newline;
+ 
+//コンストラクターにコールバック関数を渡してオブザーバーを生成
+const observer = new MutationObserver(callback);
+ 
+//監視対象の DOM（ノードや要素）この場合は body を監視
+const target = document.body;
+ 
+//監視のオプション
+const config = {
+  childList: true, //対象ノードの子ノードに対する追加・削除の監視を有効に
+  attributes: false, //対象ノードの属性に対する変更の監視を有効に
+  characterData: false, //対象ノードのテキストデータの変更の監視を有効に
+  subtree: false, //対象ノードとその子孫ノードに対する変更の監視を有効に
+};
+ 
+//observe() メソッドに監視対象と監視オプションを指定して実行（監視を開始）
+observer.observe(target, config);
+
